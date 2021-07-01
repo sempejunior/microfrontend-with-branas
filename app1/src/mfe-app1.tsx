9f41759849800e71b417ba540efc4b753c539168
carlos.sempe <carlos.sempe@zenvia.com>
2021-06-30 22:24:59 -0300

    Project created with Branas

---
+++ app1/src/mfe-app1.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import singleSpaReact from "single-spa-react";
+import Root from "./root.component";
+
+const lifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: Root,
+  errorBoundary(err, info, props) {
+    // Customize the root error boundary for your microfrontend here.
+    return null;
+  },
+});
+
+const { bootstrap: _bootstrap, mount: _mount, unmount: _unmount } = lifecycles;
+
+export function bootstrap(props) {
+  return Promise.resolve().then(() => {
+    console.log("App1 bootstrapped");
+    _bootstrap(props);
+  });
+}
+export function mount(props) {
+  return Promise.resolve().then(() => {
+    console.log("App1 mounted");
+    _mount(props);
+  });
+}
+export function unmount(props) {
+  return Promise.resolve().then(() => {
+    console.log("App1 unmounted");
+    _unmount(props);
+  });
+}
+
---
+++ navbar/src/App.tsx
@@ -0,0 +1,76 @@
+//@ts-ignore
+import * as singleSpa from 'single-spa'; 
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    if(newValue== 0){
+      singleSpa.navigateToUrl('app1');
+    }else if (newValue ==1){
+      singleSpa.navigateToUrl('app2');
+    }
+
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="App React" {...a11yProps(0)} />
+          <Tab label="App Vue" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
+}
